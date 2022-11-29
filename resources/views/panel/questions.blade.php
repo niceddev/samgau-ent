b51b5d9311cd08af287e6af9c5b374142c23e8f2
naruto <niceddeveloper@gmail.com>
2022-11-30 02:15:51 +0600

    need: finish questions edit page with delete functionality

--- resources/views/panel/questions.blade.php
+++ resources/views/panel/questions.blade.php
@@ -1,9 +1,9 @@
-@if($subjects->isEmpty())
+@if($questions->isEmpty())
     <h1 class="mt-5 text-5xl text-center font-extrabold tracking-tight leading-none text-gray-300 md:text-5xl lg:text-6xl">
         Пусто
     </h1>
 @else
-    <div class="overflow-x-auto relative">
+    <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
         <ul id="tab" class="flex flex-wrap -mb-px text-sm font-medium text-center" data-tabs-toggle="#tabContent" role="tablist">
             @foreach(config('app.languages') as $key => $lang)
                 <li class="mr-2">
@@ -17,27 +17,61 @@
         </ul>
         <div id="tabContent" id="{{ $key }}">
             @foreach(config('app.languages') as $key => $lang)
-
-                <aside id="{{ $key }}" role="tabpanel" aria-label="" style="background-color: #edeef0"
-                       class="{{ $key != app()->getLocale() ? 'hidden' : 'active' }} py-8 bg-gray-50">
-                    <div class="mx-auto max-w-screen-xl">
-                        <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-                            @foreach($subjects as $subject)
-                                <article class="bg-white p-4 rounded-lg max-w-xs text-center justify-items-center grid overflow-hidden">
-                                    <img src="{{ asset($subject->image_path) }}" class="mb-3 rounded-lg" alt="Image 1" style="max-height: 92px">
-                                    <h2 class="fs-4 font-bold leading-tight text-gray-900 text-ellipsis">
-                                        {{ $subject->getTranslation('name', $key) }}
-                                    </h2>
-                                    <a href="{{ route('platform.questions.index', $subject->id) }}"
-                                       class="mt-3 content-center grid text-white bg-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2.5">
-                                        {{ __('common.add_question') }}
+                <table id="{{ $key }}" role="tabpanel" class="{{ $key != app()->getLocale() ? 'hidden' : 'active' }} border-collapse table-auto w-full text-sm">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                        <tr>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">№</th>
+                            <th class="border-b font-medium p-4 pl-8 pb-3 text-dark-400">{{ __('common.question') }}</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">{{ __('common.more') }}</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">A</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">B</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">C</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">D</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">E</th>
+                            <th class="border-b font-medium p-4 pr-8 pb-3 text-dark-400">{{ __('common.correct_answer') }}</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white">
+                        @foreach($questions as $question)
+                            <tr>
+                                <td class="border-b border-slate-100 p-4 pl-8 text-slate-500" style="inline-size: 10px">
+                                    {{ $loop->iteration }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 pl-8 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('question', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('sub_question', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('option_a', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('option_b', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('option_c', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('option_d', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->getTranslation('option_e', $key) }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500" style="inline-size: 10px">
+                                    {{ $question->correct_answer->name }}
+                                </td>
+                                <td class="border-b border-slate-100 p-4 text-slate-500 text-right">
+                                    <a href="{{ route('platform.questions.edit', $question->id) }}"
+                                       class="hover:bg-gray-300 text-white bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+                                        {{ __('common.edit') }}
                                     </a>
-                                </article>
-                            @endforeach
-                        </div>
-                    </div>
-                </aside>
-
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             @endforeach
         </div>
     </div>
