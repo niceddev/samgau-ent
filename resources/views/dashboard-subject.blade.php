44c6e716e943cfe0f8587fe36c08d06a11293f7d
naruto <niceddeveloper@gmail.com>
2023-01-15 21:24:45 +0600

    wasd

--- resources/views/dashboard-subject.blade.php
+++ resources/views/dashboard-subject.blade.php
@@ -19,12 +19,58 @@
             {{ __('common.cabinet') }}
         </a>
     </div>
-    <div id="analyticsPage">
+    <div id="subjectAnalyticPage">
         <div class="bg-white container rounded-4" style="padding: 116px 0">
-            <div class="row">
+            <div class="row d-flex justify-content-between">
+
+                <div class="left-side bg-danger col-5">
+
+                    <p class="analytics-title fs-2 text-center">
+                        {{ $subject->getTranslation('name', session()->get('lang', 'ru')) }}
+                    </p>
+
+                </div>
+
+                <div class="right-side col-7 rounded-5 p-4" style="background-color: #A0B5C2">
+
+                    <div class="row gap-1 d-flex justify-content-around mb-4">
+                        <div class="title-side col-4" style="background-color: #A0B5C2">
+                            {{ __('common.question') }}
+                        </div>
+                        <div class="content-side col-7" >
+                            asdasda
+                        </div>
+                    </div>
+
+                    <div class="row gap-1 d-flex justify-content-around mb-4">
+                        <div class="title-side col-4">
+                            {{ __('common.answer') }}
+                        </div>
+                        <div class="content-side col-7" >
+                            asdasda
+                        </div>
+                    </div>
+
+                    <div class="row gap-1 d-flex justify-content-around mb-4">
+                        <div class="title-side col-4">
+                            {{ __('common.grade') }}
+                        </div>
+                        <div class="content-side col-7">
+                            asdasda
+                        </div>
+                    </div>
+
+                    <div class="row gap-1 d-flex justify-content-around">
+                        <div class="title-side col-4">
+                            {{ __('common.topic') }}
+                        </div>
+                        <div class="content-side col-7">
+                            asdasda
+                        </div>
+                    </div>
 
 
-
+                </div>
 
             </div>
         </div>
